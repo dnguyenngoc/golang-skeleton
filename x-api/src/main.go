6d0c7f51ab58f9688eff20e97682194446a427bf
Duy Nguyen <duynguyenngoc@hotmail.com>
2021-12-08 16:22:29 +0700

    .gitignore is now working

---
+++ x-api/src/main.go
@@ -0,0 +1,51 @@
+/**
+ * @author Duy Nguyen
+ * @email duynguyenngoc@hotmail.com
+ * @create date 2021-12-08
+ * @desc go-main init
+ */
+
+package main
+
+import (
+	_ "encoding/json"
+	_ "fmt"
+
+	"github.com/apot-group/golang-skeleton/routes"
+	"github.com/bsphere/celery"
+	"github.com/gin-gonic/gin"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq-service:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := routes.SetupRoutes()
+
+	r.GET("/test", func(c *gin.Context) {
+		task, err := celery.NewTask("x-ml.test", []string{}, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		err = task.Publish(ch, "", "x-ml")
+		if err != nil {
+			panic(err)
+		}
+
+	})
+
+	r.Run()
+}
